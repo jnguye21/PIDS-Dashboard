a1402fa998e6ef555d47333957ddff6bb9ba73b0
John <jnguye21@villanova.edu>
2020-11-18 23:00:41 -0500

    main cleanup

--- employeeParse.py
+++ employeeParse.py
@@ -1,4 +1,12 @@
 from globals import mainUser, keyList, deviceHistory, deviceCount, NUMBER_EMPLOYEES
+from datetime import datetime, timedelta
+import time, requests, os, schedule, copy, dateutil.relativedelta
+
+
+# functions in shared directory
+from dashboard import *
+from webhooks import *
+from timeRetrieval import getTimeDiff
 
 # compare seen Scanning MACs with connected Dashboard MACs
 # assign nearby employee info
@@ -18,14 +26,18 @@ def setNearbyEmployees(dashboardResponse):
                     if employee['isAway'] is True:
                         employee['firstSeen'] = client['lastSeen']
                         employee['lastSeen'] = None
+                        employee['isContinuous'] = False                        
                         employee['isAway'] = False
                         employee['justArrived'] = True
+                        employee['justLeft'] = False
                         employee['hasChanged'] = True
                     # continuous sighting
                     else:
+                        employee['lastSeen'] = None
                         employee['isContinuous'] = True
                         employee['isAway'] = False
                         employee['justArrived'] = False
+                        employee['justLeft'] = False
                         employee['hasChanged'] = False
 
                     employee['updatedSeen'] = client['lastSeen']
@@ -46,9 +58,11 @@ def setNearbyEmployees(dashboardResponse):
                                                 
                     employee['firstSeen'] = client['lastSeen']
                     employee['updatedSeen'] = client['lastSeen']
+                    employee['lastSeen'] = None
                     employee['isContinuous'] = False
                     employee['isAway'] = False
                     employee['justArrived'] = True
+                    employee['justLeft'] = False
                     employee['hasChanged'] = True
                     break
                 #else:
@@ -102,7 +116,7 @@ def setNearbyEmployees(dashboardResponse):
 def setAwayEmployees():
     # compare last in range employees
     # employees with lower updatedSeen times are 
-    for employee1 in deviceHistory['deviceList']:
+    ''' for employee1 in deviceHistory['deviceList']:
         if employee1['isMainUser'] is True:
             break
         if employee1['isAway'] is False and employee1['isMainUser'] is False:
@@ -113,6 +127,7 @@ def setAwayEmployees():
                         employee1['lastSeen'] = employee1['updatedSeen']
                         employee1['isContinuous'] = False
                         employee1['isAway'] = True
+                        employee1['justArrived'] = False
                         employee1['justLeft'] = True
                         employee1['hasChanged'] = True
                         break
@@ -121,11 +136,26 @@ def setAwayEmployees():
                         employee2['lastSeen'] = employee2['updatedSeen']
                         employee2['isContinuous'] = False
                         employee2['isAway'] = True
+                        employee2['justArrived'] = False
                         employee2['justLeft'] = True
                         employee2['hasChanged'] = True
                         break
                     else:
-                        continue
+                        continue'''
+
+    seq = [x['updatedSeen'] for x in deviceHistory['deviceList']]
+    maxTime = max(seq)
+
+    for employee in deviceHistory['deviceList']:
+        if employee['isMainUser'] is False and employee['updatedSeen'] < maxTime:
+            employee['firstSeen'] = None
+            employee['lastSeen'] = employee['updatedSeen']
+            employee['isContinuous'] = False
+            employee['isAway'] = True
+            employee['justArrived'] = False
+            employee['justLeft'] = True
+            employee['hasChanged'] = True
+
 
 def setEmployeeCount():
     global deviceCount
@@ -134,7 +164,7 @@ def setEmployeeCount():
     maxTime = max(seq)
 
     for employee in deviceHistory['deviceList']:
-        #print(employee)
+        print(employee)
 
         if employee['isMainUser'] is True:
             deviceCount += 1
@@ -172,3 +202,89 @@ def getEmployeeCount():
     employeeCountMsg += "{} other employee{} currently in the area.".format(str(deviceCount - 1), grammarInsert)
 
     return employeeCountMsg
+
+def getInfo():
+    responseBT = dashboardBT()
+    setEmployeeInfo(responseBT)
+    APInfo = getAPInfo()
+    APName = APInfo[0]['name']
+    currentEpoch = time.time()
+    currentTime = datetime.fromtimestamp(currentEpoch)
+
+    print("\n------------------------------------------------------")
+    print(currentTime)
+
+    # reorganize mainUser to front of list
+    deviceHistory['deviceList'] = sorted(deviceHistory['deviceList'], key = lambda i: 
+        (i['isMainUser'], i['justArrived'], i['justLeft']), reverse = True)
+
+    user = deviceHistory['deviceList'][0]
+    
+    #print()
+    #print(user)
+    #print()
+
+    if user['name'] is not None:        
+        selfMessage = ""
+
+        # new or returning
+        if user['justArrived'] is True:
+            user['justArrived'] = False
+            selfMessage = "You just entered the {}.".format(APName)
+        # continuously in area
+        elif user['justArrived'] is False:
+            selfMessage = "You are currently in the {}.".format(APName)
+            
+        print("{}".format(selfMessage))
+            
+    employeeCountMsg = getEmployeeCount()
+    print()
+    print("{}".format(employeeCountMsg))
+    print()
+    #sendMessage(employeeCountMsg)
+
+    #deviceHistory2 = copy.deepcopy(deviceHistory)
+
+    for employee in deviceHistory['deviceList'][1:]:
+        employeeName = employee['name']
+        #empTime = employee['updatedSeen']
+        #print(empTime)
+
+        if employeeName is not None:
+            # employee currently in range
+            if employee['isAway'] is False:
+                # new or returning
+                if employee['justArrived'] is True:
+                    nearbyEmployee = "{} just entered the {}".format(employeeName, APName)
+                    timeDiff = getTimeDiff(employee['firstSeen'], currentTime) + " ago"
+                # continuously in area
+                elif employee['isContinuous'] is True:
+                    #if employee['isMainUser'] is True:
+                    #    selfMessage = "{} are in the {}".format(employeeName, APName)
+                    #    print("{}".format(selfMessage))
+                    #    continue
+                    #else:
+                    nearbyEmployee = "{} has been in the area for".format(employeeName)
+                    timeDiff = getTimeDiff(employee['firstSeen'], currentTime)
+                
+                employeeMessage = nearbyEmployee + timeDiff + "."
+                #sendMessage(employeeMessage)
+                print("{}".format(employeeMessage))
+            # employee out of range (employee['firstSeen'] is None)
+            elif employee['isAway'] is True: #and employee['isMainUser'] is False:
+                # just left
+                #print("test1")
+                '''if employee['justLeft'] is True:
+                    #print("test2")
+                    awayEmployee = "{} just left the {}".format(employeeName, APName)
+                    employee['justLeft'] = False
+                # previously gone
+                else:'''
+                awayEmployee = "{} is no longer in the area but was last nearby".format(employeeName)
+                
+                timeDiff = getTimeDiff(employee['lastSeen'], currentTime)
+                employeeMessage = awayEmployee + timeDiff + " ago."
+                # sendMessage(employeeMessage)
+                print("{}".format(employeeMessage))
+
+    #return deviceHistory
